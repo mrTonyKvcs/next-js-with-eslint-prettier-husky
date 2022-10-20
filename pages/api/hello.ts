cee4b222f4da648db27697c4d848867cf81ed188
Kovacs Attila <kovacs.attila@arukereso.com>
2022-10-20 16:27:04 +0200

    add prettier

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string;
-};
+    name: string
+}
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ): unknown {
-  return res.status(200).json({ name: "John Doe" });
+    return res.status(200).json({ name: 'John Doe' })
 }
